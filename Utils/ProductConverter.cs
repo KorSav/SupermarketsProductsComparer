3f45567f107bd37e16657dda85235158eaa7b677
Savelii Korniiko <saveliykorniko@gmail.com>
2024-12-09 18:54:25 +0200

    extended amount qualifier to process multiplication

--- Utils/ProductConverter.cs
+++ Utils/ProductConverter.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Text.RegularExpressions;
 using program.Services.ShopsDataParsing.Enums;
 
@@ -5,23 +6,33 @@ namespace program.Utils;
 
 public static partial class ProductConverter
 {
-    [GeneratedRegex(@"([+-]?(?:[0-9]*[.,])?[0-9]*)([\p{IsCyrillic}a-zA-Z]*)")]
+    [GeneratedRegex(@"((?:\d+\*)?(?:\d+[.,])?\d*)([\p{IsCyrillic}a-zA-Z]+)")]
     private static partial Regex RatioRegex();
     public static (decimal price, MeasureType measure) GeneralizePrice(decimal price, string ratio)
     {
         var match = RatioRegex().Match(ratio);
-        if (!match.Success) throw new ArgumentException($"Failed to analyze ratio: {ratio}");
-        if (!decimal.TryParse(match.Groups[1].Value, out decimal amount))
-            amount = 1;
+        if (!match.Success) throw new ArgumentException($"Failed to analyze ratio: '{ratio}'");
+        decimal amount = 1;
+        if (match.Groups[1].Value.Trim() != "")
+            amount = EvaluateAmount(match.Groups[1].Value);
         string qualifier = match.Groups[2].Value;
         return qualifier switch
         {
             "кг" => (price / amount, MeasureType.Kilogram),
-             "г" => (price * 1000 / amount, MeasureType.Kilogram),
-             "л" => (price / amount, MeasureType.Litre),
-             "мл" => (price * 1000 / amount, MeasureType.Litre),
-             "шт" => (price / amount, MeasureType.Number),
-             _ => throw new ArgumentException($"Unknown literal {qualifier}: {amount} of {qualifier}")
+            "г" => (price * 1000 / amount, MeasureType.Kilogram),
+            "л" => (price / amount, MeasureType.Litre),
+            "мл" => (price * 1000 / amount, MeasureType.Litre),
+            "шт" => (price / amount, MeasureType.Number),
+            _ => throw new ArgumentException($"Unknown literal '{qualifier}' from '{ratio}' to specify n='{amount}' of m='{qualifier}'")
         };
+    }
+    
+    private static decimal EvaluateAmount(string expression){
+        string[] strNumbers = expression.Split('*');
+        decimal result = 1;
+        foreach (var strNumber in strNumbers){
+            result *= decimal.Parse(strNumber);
+        }
+        return result;
     }
 }
