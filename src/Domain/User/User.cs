d2d417f1733c2441384dc7a053a662d5b658b700
Savelii Korniiko <saveliykorniko@gmail.com>
2025-07-14 17:03:26 +0300

    make user type a record

--- src/Domain/User/User.cs
+++ src/Domain/User/User.cs
@@ -1,27 +1,5 @@
 namespace PriceComparer.Domain;
 
-public partial class User(int id, User.ValidName name, User.IEmail? email = null)
-{
-    public int Id { get; } = id;
-    public ValidName Name { get; set; } = name;
+public record UserId(int Value);
 
-    public interface IEmail : IEquatable<IEmail> { }
-
-    public IEmail? Email { get; set; } = email;
-
-    public (bool IsConflict, string? SamePropertyName) ConflictsWith(User other)
-    {
-        ArgumentNullException.ThrowIfNull(other);
-
-        if (Id.Equals(other.Id))
-            return (true, nameof(Id));
-
-        if (Email is null || other.Email is null)
-            return (false, null);
-
-        if (Email.Equals(other.Email))
-            return (true, nameof(Email));
-
-        return (false, null);
-    }
-}
+public record User(UserId Id, UserName Name);
